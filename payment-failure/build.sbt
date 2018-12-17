2c03721547739c67dadf21b8c355facce09ca859
Calum Campbell <campbell.cj@hotmail.co.uk>
2018-12-17 16:09:04 +0000

    add circe decoding

--- payment-failure/build.sbt
+++ payment-failure/build.sbt
@@ -3,13 +3,16 @@ name := "payment-failure-lambda"
 organization := "com.gu"
 
 scalaVersion := "2.12.6"
+val circeVersion = "0.10.1"
 
 libraryDependencies ++= Seq(
   "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0",
   "org.slf4j" % "slf4j-simple" % "1.7.25",
   "com.amazonaws" % "aws-lambda-java-core" % "1.2.0",
-  "com.amazonaws" % "aws-lambda-java-events" % "2.2.2"
-
+  "com.amazonaws" % "aws-lambda-java-events" % "2.2.2",
+  "io.circe" %% "circe-core" % circeVersion,
+  "io.circe" %% "circe-parser" % circeVersion,
+  "io.circe" %% "circe-generic" % circeVersion
 )
 
 assemblyJarName := "main.jar"
--- payment-failure/src/main/scala/com/gu/identity/paymentfailure/Lambda.scala
+++ payment-failure/src/main/scala/com/gu/identity/paymentfailure/Lambda.scala
@@ -3,22 +3,61 @@ package com.gu.identity.paymentfailure
 import com.typesafe.scalalogging.StrictLogging
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
+import io.circe.{Decoder, Encoder, Json, ParsingFailure}
+
 import scala.collection.JavaConverters._
+import io.circe.parser._
+import io.circe.generic.semiauto._
 
 
 object Lambda extends StrictLogging {
 
+  case class IdentityBrazeEmailData(externalId: String, emailAddress: String, templateId: String, customFields: Map[String, String])
+
   def handler(event: SQSEvent, context: Context): Unit = {
 
     logger.info(s"context :  $context")
-    logger.info("=========SHAPE OF INPUT=========")
-    logger.info(inputFromQueue(event))
+    logger.info(s"event :  $event")
+
+    parseMessage(event).fold(error => {
+      logger.info(s"problem parsing, failed with errors ${error.map(_.message)}")
+    }, r => {
+      logger.info(s"success with $r")
+    })
   }
 
-  private def inputFromQueue(sqsEvent: SQSEvent): String = {
+  private def parseMessage(sqsEvent: SQSEvent): Either[List[io.circe.DecodingFailure], List[IdentityBrazeEmailData]] = {
+
+    implicit val identityBrazeEmailDataDecoder: Decoder[IdentityBrazeEmailData] = deriveDecoder[IdentityBrazeEmailData]
+    implicit val identityBrazeEmailDataEncoder: Encoder[IdentityBrazeEmailData] = deriveEncoder[IdentityBrazeEmailData]
+
     val messages = sqsEvent.getRecords.asScala.map(mes => mes).toList
-    logger.info("messages")
-    logger.info(messages.toString)
-    messages.toString
+
+    val parseMessageJson: List[Either[ParsingFailure, Json]] = messages.map(mes => {
+      logger.info(s"attempting to parse message body : ${mes.getBody}")
+      parseJson(mes.getBody)
+    })
+
+    val successfullyParsedJson = parseMessageJson.collect{ case Right(v) => v}
+
+    val jsonToEmailData  = successfullyParsedJson.map(json => json.as[IdentityBrazeEmailData])
+
+    val emailData = jsonToEmailData.collect{ case Right(v) => v}
+    val decodeFailures = jsonToEmailData.collect{ case Left(failure) => failure }
+
+    decodeFailures match {
+      case Nil => Right(emailData)
+      case _ => Left(decodeFailures)
+    }
+  }
+
+  def parseJson(messageBody: String): Either[io.circe.ParsingFailure, Json] = parse(messageBody) match {
+    case Left(failure) => {
+      logger.info(s"invalid json in message : $messageBody failed with error :  ${failure.message}")
+      Left(failure)
+    }
+    case Right(json) =>
+      logger.info(s"succesfuly parsed json:  $json")
+      Right(json)
   }
 }
