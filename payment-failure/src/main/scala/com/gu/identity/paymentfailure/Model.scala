d91d7728641cbc96430122e1c6c95e244dc7cd54
Calum Campbell <campbell.cj@hotmail.co.uk>
2019-01-04 17:18:17 +0000

    simplify case class

--- payment-failure/src/main/scala/com/gu/identity/paymentfailure/Model.scala
+++ payment-failure/src/main/scala/com/gu/identity/paymentfailure/Model.scala
@@ -11,22 +11,16 @@ object IdentityBrazeEmailData {
   implicit val identityBrazeEmailDataDecoder: Decoder[IdentityBrazeEmailData] = deriveDecoder[IdentityBrazeEmailData]
 }
 
-case class BrazeRecipient(external_user_id: String, trigger_properties: Option[Map[String, String]])
+case class BrazeRecipient(external_user_id: String, trigger_properties: Map[String, String])
 
 object BrazeRecipient {
   implicit val brazeRecipientEncoder: Encoder[BrazeRecipient] = deriveEncoder[BrazeRecipient]
 }
 
-case class BrazeSendRequest(api_key: String,
-                            campaign_id: String,
-                            trigger_properties: Option[Map[String, String]],
-                            recipients: Seq[BrazeRecipient])
+case class BrazeSendRequest(api_key: String, campaign_id: String, recipients: Seq[BrazeRecipient])
 
 object BrazeSendRequest {
   implicit val BrazeSendRequestEncoder: Encoder[BrazeSendRequest] = deriveEncoder[BrazeSendRequest]
-  def apply(userId: String, apiKey: String, campaignId: String, customProperties: Map[String, String]): BrazeSendRequest = {
-    BrazeSendRequest(apiKey, campaignId, None, List(BrazeRecipient(userId, Some(customProperties))))
-  }
 }
 
 case class BrazeResponse(message: String)
