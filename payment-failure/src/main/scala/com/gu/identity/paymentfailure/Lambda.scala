cf1eb28ab42566d75edff3ea2d8eb467ef95595d
Mullefa <gccdawson@gmail.com>
2019-01-09 14:21:46 +0000

    initialise within handler

--- payment-failure/src/main/scala/com/gu/identity/paymentfailure/Lambda.scala
+++ payment-failure/src/main/scala/com/gu/identity/paymentfailure/Lambda.scala
@@ -27,18 +27,20 @@ object Lambda extends StrictLogging {
     }
   }
 
-  val config =  getConfig.valueOr(throw _)
-  val identityClient = new IdentityClient(config)
-  val sqsService = new SqsService(config)
-  val brazeClient = new BrazeClient(config)
-  val sendEmailService = new SendEmailService(identityClient, brazeClient, config)
-
   def handler(event: SQSEvent, context: Context): Unit = {
 
     logger.info(s"context :  $context")
     logger.info(s"event :  $event")
     logger.info(s"received message batch of size: ${event.getRecords.size}")
 
+    logger.info("initialising config and services")
+    val config =  getConfig.valueOr(throw _)
+    val identityClient = new IdentityClient(config)
+    val sqsService = new SqsService(config)
+    val brazeClient = new BrazeClient(config)
+    val sendEmailService = new SendEmailService(identityClient, brazeClient, config)
+
+    logger.info("config and services successfully initialised - processing events")
     process(event).foreach {
       case Left(throwable) => throw throwable
       case _ => logger.info("process successful")
