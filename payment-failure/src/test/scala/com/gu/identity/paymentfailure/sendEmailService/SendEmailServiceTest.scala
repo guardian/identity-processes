bcbcae2d815de7df17ad21441e2fda70dd279fb6
Mullefa <gccdawson@gmail.com>
2019-01-23 17:00:38 +0000

    update tests

--- payment-failure/src/test/scala/com/gu/identity/paymentfailure/sendEmailService/SendEmailServiceTest.scala
+++ payment-failure/src/test/scala/com/gu/identity/paymentfailure/sendEmailService/SendEmailServiceTest.scala
@@ -12,31 +12,62 @@ class SendEmailServiceTest extends WordSpec with Matchers with MockitoSugar {
     val identityClient = mock[IdentityClient]
     val brazeClient = mock[BrazeClient]
     val sendEmailService = new SendEmailService(identityClient, brazeClient, config)
+
+    when(config.brazeApiKey).thenReturn("braze-api-key")
   }
 
   "The sendEmail method" should {
-    "encrypt an email then trigger an email in Braze" in new TestFixture {
+
+    "trigger an email in Braze with the emailToken trigger property, if email is successfully encrypted" in new TestFixture {
 
       val emailData = IdentityBrazeEmailData("1111", "test@test.com", "templateIdMock", Map("first name" -> "test name"))
 
       when(identityClient.encryptEmail("test@test.com")).thenReturn(Right(IdentityEmailTokenResponse("OK", "encryptedEmailString")))
-      when(brazeClient.sendEmail(emailData, "encryptedEmailString")).thenReturn(Right(BrazeResponse("success")))
+
+      val brazeRequest = BrazeSendRequest(
+        api_key = "braze-api-key",
+        campaign_id = "templateIdMock",
+        recipients = List(
+          BrazeRecipient(
+            external_user_id = "1111",
+            trigger_properties = Map(
+              "first name" -> "test name",
+              "emailToken" -> "encryptedEmailString"
+            )
+          )
+        )
+      )
+
       sendEmailService.sendEmail(emailData)
 
       verify(identityClient, times(1)).encryptEmail("test@test.com")
-      verify(brazeClient, times(1)).sendEmail(emailData, "encryptedEmailString")
+      verify(brazeClient, times(1)).sendEmail(brazeRequest)
     }
 
-    "not trigger a braze email if email encryption fails" in new TestFixture {
+    "trigger an email in Braze without the emailToken trigger property, if email encryption fails" in new TestFixture {
+
       val emailData = IdentityBrazeEmailData("1111", "test@test.com", "templateIdMock", Map("first name" -> "test name"))
 
       val mockException = mock[Exception]
       when(identityClient.encryptEmail("test@test.com")).thenReturn(Left(mockException))
 
+      val brazeRequest = BrazeSendRequest(
+        api_key = "braze-api-key",
+        campaign_id = "templateIdMock",
+        recipients = List(
+          BrazeRecipient(
+            external_user_id = "1111",
+            trigger_properties = Map(
+              "first name" -> "test name"
+            )
+          )
+        )
+      )
+
       sendEmailService.sendEmail(emailData)
 
       verify(identityClient, times(1)).encryptEmail("test@test.com")
-      verify(brazeClient, never()).sendEmail(emailData, "encryptedEmailString")
+      verify(brazeClient, times(1)).sendEmail(brazeRequest)
 
     }
   }
