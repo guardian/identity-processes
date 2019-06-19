51a6a1e768a378a2449b73d54a4301a316eb6bd4
Lauren Emms <lauren.emms@guardian.co.uk>
2019-06-19 14:36:20 +0100

    change response

--- formstack-consents/src/main/scala/com/gu/identity/formstackconsents/Lambda.scala
+++ formstack-consents/src/main/scala/com/gu/identity/formstackconsents/Lambda.scala
@@ -2,21 +2,34 @@ package com.gu.identity.formstackconsents
 
 import com.gu.identity.globalConfig.DevConfig
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import io.circe.{Json, ParsingFailure}
+import scalaj.http.HttpResponse
+import io.circe.parser._
 
 object Lambda extends App {
 
   val newsletters: List[Newsletter] = List(Holidays, Students, Universities, Teachers, Masterclasses, SocietyWeekly, EdinburghFestivalDataCollection)
 
-  def handler(event: APIGatewayProxyRequestEvent): Unit = {
+  def handler(event: APIGatewayProxyRequestEvent): Either[ParsingFailure, Json] = {
     // TODO: look into integrating parameter store through cloudformation
-//    val config = new DevConfig
-//    val formstackClient = new FormstackClient(config)
-//    val identityClient = new IdentityClient(config)
-//    val lambdaService = new LambdaService(config, formstackClient, identityClient)
-//
-//    newsletters.map(lambdaService.getConsentsAndSendToIdentity)
+    //    val config = new DevConfig
+    //    val formstackClient = new FormstackClient(config)
+    //    val identityClient = new IdentityClient(config)
+    //    val lambdaService = new LambdaService(config, formstackClient, identityClient)
+    //
+    //    newsletters.map(lambdaService.getConsentsAndSendToIdentity)
     println("Hello ")
     println(event.getBody)
+    HttpResponse.apply(body = event.getBody, code = 200, headers = Map())
+    val response = """
+      |{
+      |      "isBase64Encoded": true,
+      |      "statusCode": 200,
+      |      "headers": { "headerName": "headerValue" },
+      |      "body": "body"
+      |}
+    """
+    parse(response)
   }
 }
 
