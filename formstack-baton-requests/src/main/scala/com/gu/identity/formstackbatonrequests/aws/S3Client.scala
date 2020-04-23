35a9ac4f73a6ded3794b949805cb9b4af9c180e5
Lauren Emms <lauren.emms@guardian.co.uk>
2020-04-23 10:02:20 +0100

    add write logic to s3

--- formstack-baton-requests/src/main/scala/com/gu/identity/formstackbatonrequests/aws/S3Client.scala
+++ formstack-baton-requests/src/main/scala/com/gu/identity/formstackbatonrequests/aws/S3Client.scala
@@ -1,9 +1,10 @@
 package com.gu.identity.formstackbatonrequests.aws
 
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.S3ObjectSummary
-import com.gu.identity.formstackbatonrequests.SarLambdaConfig
+import com.amazonaws.services.s3.model.{CannedAccessControlList, S3ObjectSummary}
+import com.gu.identity.formstackbatonrequests.{FormstackQuestionAnswer, FormstackSubmission, FormstackSubmissionQuestionAnswer, PerformSarLambdaConfig, SarLambdaConfig}
 import com.typesafe.scalalogging.LazyLogging
+import cats.implicits._
 
 import scala.collection.JavaConverters._
 import scala.util.Try
@@ -13,9 +14,14 @@ case class S3CompletedPathFound(resultLocations: List[String]) extends S3StatusR
 case class S3FailedPathFound() extends S3StatusResponse
 case class S3NoResultsFound() extends S3StatusResponse
 
+case class S3WriteSuccess()
+
 
 trait S3Client {
   def checkForResults(initiationId: String, config: SarLambdaConfig): Either[Throwable, S3StatusResponse]
+  def writeSuccessResult(initiationId: String, results: List[FormstackSubmissionQuestionAnswer], config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess]
+  def writeFailedResults(initiationId: String, err: String, config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess]
+  def copyResultsToCompleted(initiationId: String, config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess]
 }
 
 object S3 extends S3Client with LazyLogging {
@@ -63,5 +69,64 @@ object S3 extends S3Client with LazyLogging {
       completedResults <- listFolderContents(config.resultsBucket, completedPath)
       failedResults <- listFolderContents(config.resultsBucket, failedPath)
     } yield s3StatusResponse(failedResults, completedResults, config.resultsBucket)
+  }
+
+  private def writeToS3(resultsBucket: String, filePath: String, contents: String): Either[Throwable, S3WriteSuccess] = Try {
+    s3Client.putObject(resultsBucket, filePath, contents)
+    s3Client.setObjectAcl(resultsBucket, filePath, CannedAccessControlList.BucketOwnerRead)
+  }.toEither.map(_ => S3WriteSuccess())
+
+  private def formatResults(results: List[FormstackSubmissionQuestionAnswer]): String = {
+    results.map { submission =>
+      val meta = List(s"Submission: ${submission.id}", s"Timestamp: ${submission.timestamp}")
+      val responses = submission.fields.map(field => s"${field.label}: ${field.value}")
+      (meta ::: responses).mkString("\n")
+    }.mkString("\n-------------------------\n")
+  }
+
+  override def writeSuccessResult(
+    initiationId: String,
+    results: List[FormstackSubmissionQuestionAnswer],
+    config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess] = {
+    val resultsPath = s"${config.resultsPath}/$initiationId/pending"
+    if (results.nonEmpty) {
+      val filePath = s"$resultsPath/formstackSarResponse"
+      logger.info("Writing SAR result to s3.")
+      val contents = formatResults(results)
+      writeToS3(config.resultsBucket, filePath, contents)
+    } else Right(S3WriteSuccess())
+  }
+
+  override def writeFailedResults(
+    initiationId: String,
+    err: String,
+    config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess] = {
+    val filePath = s"${config.resultsPath}/$initiationId/failed/formstackSarFailed"
+    logger.info("Writing to failed path in s3.")
+    writeToS3(config.resultsBucket, filePath, err)
+  }
+
+  private def copyToS3(resultsBucket: String, from: String, to: String): Either[Throwable, Unit] = Try {
+    s3Client.copyObject(resultsBucket, from, resultsBucket, to)
+    s3Client.setObjectAcl(resultsBucket, to, CannedAccessControlList.BucketOwnerRead)
+  }.toEither
+
+  override def copyResultsToCompleted(initiationId: String, config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess] = {
+    val pendingPath = s"${config.resultsPath}/$initiationId/pending/"
+    val pendingObjects = listFolderContents(config.resultsBucket, pendingPath)
+    pendingObjects.flatMap { pendingObjects =>
+      if (pendingObjects.isEmpty) {
+        val filePath = s"${config.resultsPath}/$initiationId/completed/noResultsFoundForUser"
+        logger.info("No results found in /pending. Creating NoResultsFoundForUser object.")
+        writeToS3(config.resultsBucket, filePath, "")
+      } else {
+        pendingObjects.traverse { obj =>
+          val pendingPath = obj.getKey
+          val completedPath = obj.getKey.replace("pending", "completed")
+          logger.info("Copying results from /pending to /completed.")
+          copyToS3(config.resultsBucket, pendingPath, completedPath)
+        }.map(_ => S3WriteSuccess())
+      }
+    }
   }
 }
--- formstack-baton-requests/src/test/scala/com/gu/identity/formstackbatonrequests/S3ClientStub.scala
+++ formstack-baton-requests/src/test/scala/com/gu/identity/formstackbatonrequests/S3ClientStub.scala
@@ -1,11 +1,17 @@
 package com.gu.identity.formstackbatonrequests
 
-import com.gu.identity.formstackbatonrequests.aws.{S3Client, S3CompletedPathFound, S3FailedPathFound, S3NoResultsFound, S3StatusResponse}
+import com.gu.identity.formstackbatonrequests.aws.{S3Client, S3CompletedPathFound, S3FailedPathFound, S3NoResultsFound, S3StatusResponse, S3WriteSuccess}
 
 class S3ClientStub (
-  checkForResultsResponse: Either[Throwable, S3StatusResponse]
+  checkForResultsResponse: Either[Throwable, S3StatusResponse],
+  writeSuccessResultsResponse: Either[Throwable, S3WriteSuccess],
+  writeFailedResultsResponse: Either[Throwable, S3WriteSuccess],
+  copyResultsResponse: Either[Throwable, S3WriteSuccess]
 ) extends S3Client {
   override def checkForResults(initiationId: String, config: SarLambdaConfig): Either[Throwable, S3StatusResponse] = checkForResultsResponse
+  override def writeSuccessResult(initiationId: String, results: List[FormstackSubmissionQuestionAnswer], config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess] = writeSuccessResultsResponse
+  override def writeFailedResults(initiationId: String, err: String, config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess] = writeFailedResultsResponse
+  override def copyResultsToCompleted(initiationId: String, config: PerformSarLambdaConfig): Either[Throwable, S3WriteSuccess] = copyResultsResponse
 }
 
 object S3ClientStub {
@@ -13,7 +19,10 @@ object S3ClientStub {
   val failedPathFoundResponse: Either[Throwable, S3StatusResponse] = Right(S3FailedPathFound())
   val noResultFoundResponse: Either[Throwable, S3StatusResponse] = Right(S3NoResultsFound())
 
-  def withSuccessResponse = new S3ClientStub(successfulPathFoundResponse)
-  def withFailedResponse = new S3ClientStub(failedPathFoundResponse)
-  def withPendingResponse = new S3ClientStub(noResultFoundResponse)
+  val successfullyWroteToS3Response = Right(S3WriteSuccess())
+  val failedToWriteToS3Response = Left(new Exception("S3 error"))
+
+  def withSuccessResponse = new S3ClientStub(successfulPathFoundResponse, successfullyWroteToS3Response, successfullyWroteToS3Response, successfullyWroteToS3Response)
+  def withFailedResponse = new S3ClientStub(failedPathFoundResponse, failedToWriteToS3Response, failedToWriteToS3Response, failedToWriteToS3Response)
+  def withPendingStatusResponse = new S3ClientStub(noResultFoundResponse, successfullyWroteToS3Response, successfullyWroteToS3Response, successfullyWroteToS3Response)
 }
