1fbf834e67da1b065544f86ba6fb019c0b50fd53
Patricio Vighi <pvighi@gmail.com>
2023-09-28 17:56:32 +0100

    do not fail if there's no tests for a package
    TODO: maybe undo later when the cdk tests are configured?

--- access-count-fetcher/jest.config.js
+++ access-count-fetcher/jest.config.js
@@ -3,6 +3,15 @@ module.exports = {
 	testEnvironment: 'node',
 	projects: [
 		{
+			displayName: 'cdk',
+			transform: {
+				'^.+\\.tsx?$': 'ts-jest',
+			},
+			setupFilesAfterEnv: ['<rootDir>/packages/cdk/jest.setup.js'],
+			testMatch: ['<rootDir>/packages/cdk/**/*.test.ts'],
+
+		},
+		{
 			displayName: 'lambda',
 			transform: {
 				'^.+\\.tsx?$': 'ts-jest',
