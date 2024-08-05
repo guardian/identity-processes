8fc030e64cfaa6da14a46e2c0d9e32d43a16fb60
Patricio Vighi <pvighi@gmail.com>
2024-08-05 15:36:25 +0100

    Pv/snyk 2024 07 31 attempt 2 (#272)
    * update dependencies
    
    * replace docker-compose with docker compose
    
    docker-compose doesn't work in the new version of docker that github actions use

--- formstack-baton-requests/localenv/start-dependencies.sh
+++ formstack-baton-requests/localenv/start-dependencies.sh
@@ -4,4 +4,4 @@ set -ex
 cd `dirname $0`
 
 ./stop-dependencies.sh
-docker-compose -p formstack-baton-requests up --force-recreate -d
+docker compose -p formstack-baton-requests up --force-recreate -d
