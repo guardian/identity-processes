b135e8eab9c1452928f64d6708fa2db705a01ec6
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2026-04-14 09:30:25 +0100

    Remove formstack-baton-requests project

--- formstack-baton-requests/src/main/scala/com/gu/identity/formstackbatonrequests/services/Util.scala
+++
@@ -1,9 +0,0 @@
-package com.gu.identity.formstackbatonrequests.services
-
-object Util {
-  def extractEmails(txt: String): List[String] = {
-    val emailReg = """(?i)\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}\b""".r
-    emailReg.findAllIn(txt).toList
-  }
-}
-
